cf3f5cd9ac234b92644b2cc3bce0c543b6f67aec
KESHIA VEREDA <vereda761@gmail.com>
2020-08-23 23:27:17 -0700

    path not working

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -42,4 +42,28 @@ def scrape():
     data = hemispheres_soup.find_all("div", class_="item")
     hemisphere_img_urls = []
 
+    for d in data:
     
+        title = d.find("h3").text
+
+        img_url = d.a["href"]
+        url = "https://astrogeology.usgs.gov" + img_url
+        response = requests.get(url)
+        
+        soup = bs(response.text,"html.parser")
+        mars_url = soup.find("img", class_="wide-image")["src"]
+        full_url = "https://astrogeology.usgs.gov" + mars_url
+        
+        hemisphere_img_urls.append({"title": title, "img_url": full_url})
+
+    #dictionary
+    mars_data = {
+        "news_title": news_title,
+        "news_p" : news_p,
+        "featured_image_url": featured_image_url,
+        "mars_html_table": mars_html_table,
+        "hemisphere_img_urls": hemisphere_img_urls
+    }
+
+    browser.quit()
+    return mars_data
